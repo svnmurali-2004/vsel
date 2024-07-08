f823fbdd7f66c016abf91b0036b2a7b1020efed9
svnmurali-2004 <123376873+svnmurali-2004@users.noreply.github.com>
2024-07-08 10:41:36 +0530

    initial commit 3

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ app.use(cors())
 app.get("/",async(req,res)=>{
     res.send({ok:true ,msg:"hello World"})
 })
-
+app.get("/hello",async(req,res)=>{
+    res.send({ok:true ,msg:"hello World"})
+})
 app.listen(5000,()=>{
     console.log("server is running at port 5000")
 })
